1c8ec359f1fed0f32e310bf1c06fb7e3a57c4d9f
Карина <sychevaakarina@gmail.com>
2016-11-27 11:57:39 +0300

    some changes

---
+++ src/main/java/ru/aplana/autotests/steps/AbstractStep.java
@@ -0,0 +1,11 @@
+package ru.aplana.autotests.steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AbstractStep {
+
+    protected WebDriver driver;
+
+
+}
--- src/main/java/ru/aplana/autotests/steps/GoogleSteps.java
+++ src/main/java/ru/aplana/autotests/steps/GoogleSteps.java
@@ -7,13 +7,14 @@ import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.junit.Assert;
+import org.openqa.selenium.chrome.ChromeDriver;
 import ru.aplana.autotests.pages.HomePage;
 import ru.aplana.autotests.pages.LoginPage;
 
 /**
  * Created by aplana on 24.11.16.
  */
-public class GoogleSteps {
+public class GoogleSteps extends AbstractStep{
 
     @Steps
     GoogleScenarioSteps googleScenarioSteps;
@@ -21,9 +22,8 @@ public class GoogleSteps {
     @Given("открыта главная страница")
     public void openHomePage(){
         String workingDir = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         String baseUrl = "https://www.google.ru/";
-        ThucydidesWebDriverSupport.getDriver().get(baseUrl);
+        driver = new ChromeDriver();
     }
     @When("выполнен переход к странице авторизации")
     public void goToLoginGmailPage(){
